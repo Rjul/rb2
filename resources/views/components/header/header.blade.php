1b6fa4ff8818d48c76680add526c0dbd46e6accf
r-sede <r_sede@orange.fr>
2022-11-26 22:14:27 +0100

    inte header

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,29 +1,29 @@
 <header>
 
-    <div class="header">
-        <div><img class="header_logo-rb" srcset="/imgs/logo.png" alt="logo Radiobastides"/></div>
-        <nav>
-            <ul>
-                @foreach($groupProgrammes as $category)
-                    <li>
-                        <x-link-dropdown :category="$category"/>
-                    </li>
-                @endforeach
-            </ul>
-        </nav>
-        <x-header-search></x-header-search>
-    </div>
-    @if(!empty($websiteNews))
-        <div id="header_annonce" class="text-container">
-            <span class="target">
-                @foreach($websiteNews as $websiteNew)
-                # {{ $websiteNew->content }}
-                @endforeach
-            </span>
-            <div class="fader fader-left"></div>
-            <div class="fader fader-right"></div>
-        </div>
-    @endif
+{{--    <div class="header">--}}
+{{--        <div><img class="header_logo-rb" srcset="/imgs/logo.png" alt="logo Radiobastides"/></div>--}}
+{{--        <nav>--}}
+{{--            <ul>--}}
+{{--                @foreach($groupProgrammes as $category)--}}
+{{--                    <li>--}}
+{{--                        <x-link-dropdown :category="$category"/>--}}
+{{--                    </li>--}}
+{{--                @endforeach--}}
+{{--            </ul>--}}
+{{--        </nav>--}}
+{{--        <x-header-search></x-header-search>--}}
+{{--    </div>--}}
+{{--    @if(!empty($websiteNews))--}}
+{{--        <div id="header_annonce" class="text-container">--}}
+{{--            <span class="target">--}}
+{{--                @foreach($websiteNews as $websiteNew)--}}
+{{--                # {{ $websiteNew->content }}--}}
+{{--                @endforeach--}}
+{{--            </span>--}}
+{{--            <div class="fader fader-left"></div>--}}
+{{--            <div class="fader fader-right"></div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
 
     <nav class="navbar navbar-expand-lg bg-white px-lg-4">
         <div class="container-fluid">
