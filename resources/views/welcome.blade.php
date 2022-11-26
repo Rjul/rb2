96fec9533acea25ad2782f6903086554707a899b
Hummel <juhummel@laposte.net>
2022-11-26 21:30:45 +0100

    User route base layout

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('base')
+
+@push('body')
+
+    <div class="app-container" >
+        <div class="section" >
+            <x-last-home type="audio"></x-last-home>
+
+            <x-last-home type="text"></x-last-home>
+
+            <x-last-home type="video"></x-last-home>
+        </div>
+        <div class="section" >
+
+        </div>
+    </div>
+
+
+@endpush
