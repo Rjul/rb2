67c7f5f8a1adc6dd3b98c01143b24f53f8adbf32
Hummel <juhummel@laposte.net>
2022-12-04 12:14:59 +0100

    Mp3 duration and command

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use FFMpeg\FFProbe;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,44 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('applied:duration', function () {
+    $this->comment("Get all attachment without duration");
+    $attachments = \App\Models\Attachment::query()->where('duration', null)->get();
+
+    $this->comment(sprintf('%s found', count($attachments)));
+    $attachments->each(function ($attachment) {
+        $file =  __DIR__.'/../storage/app/public/emission/audio/'. $attachment->path . $attachment->name .'.'. $attachment->extension;
+        $mp3 = new \App\Utilities\MP3File( __DIR__.'/../storage/app/public/emission/audio/'. $attachment->path . $attachment->name .'.'. $attachment->extension);
+        dump($file);
+        dump($mp3->getDuration() / 60);
+        dump($mp3->getDurationEstimate() / 60);
+
+        if (file_exists($file)){
+            ## open and read video file
+            $handle = fopen($file, "r");
+
+            ## read video file size
+            $contents = fread($handle, filesize($file));
+            fclose($handle);
+            $make_hexa = hexdec(bin2hex(substr($contents,strlen($contents)-3)));
+            if (strlen($contents) > $make_hexa){
+                $pre_duration = hexdec(bin2hex(substr($contents,strlen($contents)-$make_hexa,3))) ;
+                $post_duration = $pre_duration/1000;
+                $timehours = $post_duration/3600;
+                $timeminutes =($post_duration % 3600)/60;
+                $timeseconds = ($post_duration % 3600) % 60;
+                $timehours = explode(".", $timehours);
+                $timeminutes = explode(".", $timeminutes);
+                $timeseconds = explode(".", $timeseconds);
+                $duration = $timehours[0]. ":" . $timeminutes[0]. ":" . $timeseconds[0];
+            }
+            $attachment->duration = $duration ?? null;
+            dump($duration ?? null);
+        } else {
+            $attachment->duration = null;
+            dump('nan');
+        }
+    });
+
+})->purpose('Get all attachment without duration and try to get this');
