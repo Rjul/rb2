1525cdf65224aae8b3f1c41419621a515a5c54d4
r-sede <r_sede@orange.fr>
2023-01-15 23:29:08 +0100

    inte programme

--- resources/views/components/cards/xl-w-card.blade.php
+++ resources/views/components/cards/xl-w-card.blade.php
@@ -9,29 +9,28 @@
                              class="item-card_img item-card_img--small lazyload img-fluid">
                     </div>
                     @if ($emision->media_type == 'audio' or $emision->media_type == 'video')
-                        <div class="item-card_content_length--small mt-4">{{ $emision->duration }}</div>
+                        <div class="item-card_content_length--xl mt-4">{{ $emision->duration }}</div>
                     @endif
 
                 </div>
                 <div class="col-sm-6 col-12 ">
                     <div class="item-card_content-wrapper">
-                        <h3 class="item-card_content-title--small">{{ $emision->name }}</h3>
-                        <p class="item-card_content-txt--small">{{ Str::words(strip_tags(Str::limit($emision->description, 400))) }}</p>
+                        <h3 class="item-card_content-title--xl">{{ $emision->name }}</h3>
+                        <p class="item-card_content-txt--xl">{{ Str::words(strip_tags(Str::limit($emision->description, 300))) }}</p>
                     </div>
-
                 </div>
 
             </div>
             <div class="row mt-auto">
-                <div class="col-9 h-100">
+                <div class="col-6 h-100">
                     <div class="item-card--xl_tag-wrapper mt-3">
                         @foreach($emision->tags as $tag)
-                            <x-tag :$tag :isLarge="false" ></x-tag>
+                            <x-tag :$tag :isLarge="true" ></x-tag>
                         @endforeach
                     </div>
                 </div>
-                <div class="col-3 h-100">
-                    <div class="item-card--xl_cta-wrapper">
+                <div class="col-6 h-100">
+                    <div class="item-card--xl_cta-wrapper mt-3">
                         <a href="{{ route('view-emision', [ 'programme' => $emision->programme, 'emision' => $emision ]) }}" class="item-card--xl_cta {{$emision->media_type}}">
                             @if ($emision->media_type == 'audio')
                                 Ecouter
