78e5e8e9875aa6e6eee3a833dc13b832f8445d35
Hummel <juhummel@laposte.net>
2023-03-25 16:38:47 +0100

    Search engine icon type

--- resources/views/components/list/search-engine.blade.php
+++ resources/views/components/list/search-engine.blade.php
@@ -8,14 +8,34 @@
                 <div class="d-flex flex-wrap justify-content-around mt-3">
                     @foreach($types as $key => $type)
                         <div class="position-relative m-1 ">
-                            <label id="label-tag-{{ $type }}" class="label-search-facet-elms flex-center h-100 form-check fs-4 label btn btn-primary btn--small
+                            <label id="label-tag-{{ $type }}" class="label-search-facet-elms flex-center h-100 form-check fs-2 label btn btn-outline-secondary
                                         {{ in_array($type, request('type', [])) ? ' active border border-quadrary border-3' : '' }}
                                         " for="type-{{ $type }}"
+                                   style="color: #131e25"
                             >
                                 <input class="position-absolute check-box__elm-search-engin left right d-none" type="checkbox" name="type[]" value="{{ $type }}" id="type-{{ $type }}"
                                     {{ in_array($type, request('type', [])) ? 'checked' : '' }}
                                 >
-                                <div class="">{{ $key }}</div>
+                                <div class="">{{--{{ $key }}--}}
+                                @if($type === \App\Models\Emision::TYPE_AUDIO)
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-music-note-beamed" viewBox="0 0 16 16">
+                                        <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13c0-1.104 1.12-2 2.5-2s2.5.896 2.5 2zm9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2z"/>
+                                        <path fill-rule="evenodd" d="M14 11V2h1v9h-1zM6 3v10H5V3h1z"/>
+                                        <path d="M5 2.905a1 1 0 0 1 .9-.995l8-.8a1 1 0 0 1 1.1.995V3L5 4V2.905z"/>
+                                    </svg>
+                                @elseif($type === \App\Models\Emision::TYPE_VIDEO)
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-camera-reels" viewBox="0 0 16 16">
+                                        <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM1 3a2 2 0 1 0 4 0 2 2 0 0 0-4 0z"/>
+                                        <path d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7zm6 8.73V7.27l-3.5 1.555v4.35l3.5 1.556zM1 8v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1z"/>
+                                        <path d="M9 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM7 3a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                                    </svg>
+                                @elseif($type === \App\Models\Emision::TYPE_TEXT)
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-chat-left-text" viewBox="0 0 16 16">
+                                        <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                        <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                                    </svg>
+                                @endif
+                                </div>
                             </label>
                         </div>
                     @endforeach
