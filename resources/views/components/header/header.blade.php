2ddaaeee59d4b715f237181be1e1aa71deb466d5
Hummel <juhummel@laposte.net>
2023-05-13 13:27:16 +0200

    suggestion position intégration

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -1,30 +1,4 @@
 <header class="header fixed-top">
-
-{{--    <div class="header">--}}
-{{--        <div><img class="header_logo-rb" srcset="/imgs/logo.png" alt="logo Radiobastides"/></div>--}}
-{{--        <nav>--}}
-{{--            <ul>--}}
-{{--                @foreach($groupProgrammes as $category)--}}
-{{--                    <li>--}}
-{{--                        <x-link-dropdown :category="$category"/>--}}
-{{--                    </li>--}}
-{{--                @endforeach--}}
-{{--            </ul>--}}
-{{--        </nav>--}}
-{{--        <x-header-search></x-header-search>--}}
-{{--    </div>--}}
-{{--    @if(!empty($websiteNews))--}}
-{{--        <div id="header_annonce" class="text-container">--}}
-{{--            <span class="target">--}}
-{{--                @foreach($websiteNews as $websiteNew)--}}
-{{--                # {{ $websiteNew->content }}--}}
-{{--                @endforeach--}}
-{{--            </span>--}}
-{{--            <div class="fader fader-left"></div>--}}
-{{--            <div class="fader fader-right"></div>--}}
-{{--        </div>--}}
-{{--    @endif--}}
-
     <nav class="navbar navbar-expand-lg bg-white px-lg-4">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{ route('homepage') }}">
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,10 +1,10 @@
 <div class="modal fade" id="exampleModal" tabindex="99" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-xl">
         <div class="modal-content mw-100">
-            <div class="modal-header w-100">
-                <div class="m-3 d-flex align-items-end">
+            <div class="position-relative modal-header w-100">
+                <div class="m-3 w-100 d-flex align-items-center justify-content-between">
                     <label class="modal-title" id="exampleModalLabel">Recherche</label>
-                    <input id="input-suggestion" type="text" class="form-control-search form-control m-3" value="{{ request('query') }}">
+                    <input id="input-suggestion" type="text" class="w-100 form-control-search form-control m-3" value="{{ request('query') }}">
                     <button class="m-3 btn btn-primary pe-auto"><i id="search-btn" class="fa fa-search ">
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
@@ -12,7 +12,7 @@
                         </i>
                     </button>
                 </div>
-                <button type="button" class="btn-close p-3" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close p-3 suggestion__close-btn" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div id="place-suggestion" class="p-3">
