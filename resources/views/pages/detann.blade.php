0e809087f078df3e4192bb7ca93783915ac6935c
Hummel <juhummel@laposte.net>
2023-05-13 13:46:28 +0200

    detann.scss intégration

--- resources/views/pages/detann.blade.php
+++ resources/views/pages/detann.blade.php
@@ -21,31 +21,43 @@
 
                         <img src="{{ $emision->image }}" class="img-full w-100 rounded-bottom" alt="...">
                     </div>
-                    <section class="article__administrable_content">
+
+                    <section class="article__administrable_content mb-3">
                         <p class="card-text">{!! $emision->description !!}</p>
                     </section>
-                    <div class="detann__down_image" >
-                        <svg class="svg__flech svg__flech_reversed" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080">
-                            <defs>
-                                <style>
-                                    .cls-1 {
-                                        fill: #000000;
-                                    }
-                                </style>
-                            </defs>
-                            <path id="Tracé_11" data-name="Tracé 11" class="cls-1" d="M342.89,770.24l-3.05-460.48,400.31,227.61-397.26,232.87Z"/>
-                        </svg>
-                        <svg class="svg__flech" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080">
-                            <defs>
-                                <style>
-                                    .cls-1 {
-                                        fill: #000000;
-                                    }
-                                </style>
-                            </defs>
-                            <path id="Tracé_11" data-name="Tracé 11" class="cls-1" d="M342.89,770.24l-3.05-460.48,400.31,227.61-397.26,232.87Z"/>
-                        </svg>
-                    </div>
+
+                    <section class="detann__down_image mt-5">
+                        <h2 class="m-3 h1">Poursuivre parmi les émissions du programme <u>{{ $emision->programme->name }}</u></h2>
+                        <div class="d-flex flex-row justify-content-around align-items-center">
+                            <div class="detann__next-and-preview">
+                                <h3 class="detann__next_title h2">{{ $emision->name }}</h3>
+                                <svg class="svg__flech svg__flech_reversed" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080">
+                                    <defs>
+                                        <style>
+                                            .cls-1 {
+                                                fill: #000000;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <path id="Tracé_11" data-name="Tracé 11" class="cls-1" d="M342.89,770.24l-3.05-460.48,400.31,227.61-397.26,232.87Z"/>
+                                </svg>
+                            </div>
+                            <div class="detann__next-and-preview">
+                                <h3 class="detann__preview_title h2">{{ $emision->name }}</h3>
+                                <svg class="svg__flech" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080">
+                                    <defs>
+                                        <style>
+                                            .cls-1 {
+                                                fill: #000000;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <path id="Tracé_11" data-name="Tracé 11" class="cls-1" d="M342.89,770.24l-3.05-460.48,400.31,227.61-397.26,232.87Z"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </section>
+
                     <div class="commentaire">
                         {{-- view/vendor/comments --}}
                         {{-- 3 files --}}
