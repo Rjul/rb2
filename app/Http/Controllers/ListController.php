21677904667033ee86d58e2dc8b66db74caefbf5
Hummel <juhummel@laposte.net>
2023-11-25 18:18:32 +0100

    fix

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -19,8 +19,9 @@ class ListController extends Controller
     {
         $emisions = Emision::query()->with(['programme']);
         $emisions->orderBy('active_at', 'desc')
-            ->where('active_at', '<', now())
-            ->where('active', "=", true);
+            ->where('active_at', '<', now());
+//            ->where('active', "=", true)
+;
 
         if (!is_null($programme)) {
             $emisions
