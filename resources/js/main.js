3b95029d9373b7668824a00201dd920d2e5beb4f
Hummel <juhummel@laposte.net>
2023-12-10 11:42:54 +0100

    maj style

--- resources/js/main.js
+++ resources/js/main.js
@@ -17,6 +17,10 @@ Alpine.start()
 let containerAnnonces = document.querySelector("#header_annonce");
 let text = document.querySelector("#header_annonce span");
 if (containerAnnonces) {
+  console.log(document.documentElement.clientWidth)
+  console.log(text.clientWidth)
+  console.log(window.clientWidth < text.clientWidth)
+  console.log(window.clientWidth < text.clientWidth)
     if (containerAnnonces.clientWidth < text.clientWidth) {
         text.classList.add("animate");
     }
