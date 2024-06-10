ac930251a7777cfaffda6030874479903f62e55a
julien <hummel.julien.pro@gmail.com>
2024-06-10 09:00:45 +0200

    default false

---
+++ database/migrations/2024_06_08_101806_default_to_programme.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('emisions', function (Blueprint $table) {
+            // change default value of approved column
+            $table->boolean('is_active')->default(false)->change();
+        });
+        Schema::table('programmes', function (Blueprint $table) {
+            // change default value of approved column
+            $table->boolean('is_active')->default(false)->change();
+        });
+        Schema::table('group_programmes', function (Blueprint $table) {
+            // change default value of approved column
+            $table->boolean('is_active')->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('programmes', function (Blueprint $table) {
+            $table->removeColumn('has_rss');
+        });
+    }
+};
