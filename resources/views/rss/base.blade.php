9ca9f80548b9f1527cd2925053a5a98a302b8690
julien <hummel.julien.pro@gmail.com>
2024-07-21 11:58:37 +0200

    fix programme name

--- resources/views/rss/base.blade.php
+++ resources/views/rss/base.blade.php
@@ -1,7 +1,7 @@
 @php echo '<?xml version="1.0" encoding="UTF-8"?>'@endphp
 <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
     <channel>
-        <title>Podcast Dafna et ses zèbres</title>
+        <title>{{ $programme->name }}</title>
         <itunes:owner>
             <itunes:email>{{ $programme->user->email }}</itunes:email>
         </itunes:owner>
