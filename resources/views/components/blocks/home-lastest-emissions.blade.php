6327b0a286eb91caad33f41c9bda5ca9c57e6f88
julien <hummel.julien.pro@gmail.com>
2025-09-08 17:57:36 +0200

    fix mobile

--- resources/views/components/blocks/home-lastest-emissions.blade.php
+++ resources/views/components/blocks/home-lastest-emissions.blade.php
@@ -1,5 +1,5 @@
 @if (!$emissions->isEmpty())
-    <div class="d-flex flex-row mt-3 flex-wrap">
+    <div class="mt-3 flex-wrap">
         <div class="row gy-3">
             @foreach($emissions as $emision)
                 @if ($loop->first)
